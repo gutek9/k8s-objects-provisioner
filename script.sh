4feac133214570edc5c46bfecafe3b77ae1f128b
Lukasz Bielinski <l.bielinski@oberthur.com>
2016-07-28 13:31:00 +0200

    process only modified files

--- script.sh
+++ script.sh
@@ -11,9 +11,38 @@ cleanup ()
 
 trap cleanup SIGINT SIGTERM
 
+touch /tmp/filelist.txt || exit
+
 while [ 1 ]
 do
   sleep 15 &
   wait $!
-  kubectl apply -f /src/$NAMESPACE --recursive
+  find /src/$NAMESPACE   -type f -not -path "*.git*"  -exec md5sum {} +   > /tmp/filelist.new.txt
+   comm -1 -3 <(sort /tmp/filelist.txt) <(sort /tmp/filelist.new.txt) > /tmp/filelist.process.txt
+
+  while read line
+  do
+      # echo -e "$line \n"
+      SUBSTRING=$(echo $line|  cut -d' '  -f2)
+      echo  "File $SUBSTRING has changed, processing at $date"
+      kubectl apply -f $SUBSTRING
+  done < /tmp/filelist.process.txt
+
+
+  mv /tmp/filelist.new.txt > /tmp/filelist.txt
+
 done
+
+
+
+# 1) lista plikow z hashami
+# 2) nowa lista porownanie listy
+#
+# lista
+# lista.new
+#
+# porownanie -> lista.process
+# mv lista.new -> lista
+#
+# 3) process na plikach ktore zmienily hashe
+# 4) process parallel
