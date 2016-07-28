2afd6af8e7df09b47b9f015801d22290cf6fe029
Lukasz Bielinski <l.bielinski@oberthur.com>
2016-07-28 13:59:33 +0200

    find only yaml files

--- script.sh
+++ script.sh
@@ -17,13 +17,14 @@ while [ 1 ]
 do
   sleep 15 &
   wait $!
-  find /src/$NAMESPACE   -type f -not -path "*.git*"  -exec md5sum {} +   > /tmp/filelist.new.txt
+  find /src/$NAMESPACE   -type f -name *.yaml -not -path "*.git*"  -exec md5sum {} +   > /tmp/filelist.new.txt
    comm -1 -3 <(sort /tmp/filelist.txt) <(sort /tmp/filelist.new.txt) > /tmp/filelist.process.txt
 
   while read line
   do
       # echo -e "$line \n"
       SUBSTRING=$(echo $line|  cut -d' '  -f2)
+      date=$(date --iso-8601=seconds)
       echo  "File $SUBSTRING has changed, processing at $date"
       kubectl apply -f $SUBSTRING
   done < /tmp/filelist.process.txt
