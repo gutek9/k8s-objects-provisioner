2950bdb7482722dd6b78b16d68c1107b1a046760
pulse <antynomia@gmail.com>
2017-03-24 20:55:13 +0100

    nothing important

--- provisioners/script-generic.sh
+++ provisioners/script-generic.sh
@@ -11,21 +11,21 @@ cleanup ()
 trap cleanup SIGINT SIGTERM
 
 while [ 1 ]
-  do
-    sleep $[ ( $RANDOM % 20 )  + 1 ]s &
-    wait $!
+do
+  sleep $[ ( $RANDOM % 20 )  + 1 ]s &
+  wait $!
 
-      if ( set -o noclobber; echo $PROV_TYPE > "$lockfile") 2> /dev/null; then
-        trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
+  if ( set -o noclobber; echo $PROV_TYPE > "$lockfile") 2> /dev/null; then
+    trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
 
-        func_apply_on_changed_files $DEPLOYMENT_DIR
+    func_apply_on_changed_files $DEPLOYMENT_DIR
 
-        # clean up after yourself, and release your trap
-        rm -f "$lockfile"
-        trap - INT TERM EXIT
-      else
-        date=$(date --iso-8601=seconds)
-        echo "$date Lock Exists: $lockfile owned by $(cat $lockfile)"
-      fi
+    # clean up after yourself, and release your trap
+    rm -f "$lockfile"
+    trap - INT TERM EXIT
+  else
+    date=$(date --iso-8601=seconds)
+    echo "$date Lock Exists: $lockfile owned by $(cat $lockfile)"
+  fi
 
-  done
+done
