887a4332c8f0153ffc49faae0cc35e637ff1a3b6
Adamek, Mateusz <Mateusz.Adamek@harman.com>
2020-03-20 18:27:41 +0100

    provisioner with branches as ns

--- provisioners/provisioner.sh
+++ provisioners/provisioner.sh
@@ -12,124 +12,101 @@ function lock {
 
 function remove_lock {
     rm -rf $LOCK_DIR
+    exit 2
 }
 
 trap remove_lock SIGINT
 trap remove_lock KILL
-trap remove_lock ERROR
 
 function get_data {
-                f=$1
-                ns=$( echo $f | cut -d "/" -f3 )
-                if [[ $f == *"rcs"* ]]; then
-                        pod=$( echo $f | cut -d "/" -f5 | cut -d "." -f1)
-                else 
-                        pod=$( echo $f | cut -d "/" -f4 )
-                fi
+    f=$1
+    
+    objectpath=$( dirname $f )
+    objectname=$( echo $objectpath | rev | cut -d "/" -f1 | rev )
 }
 
-function deploysecrets {
-                local sec=""        
-                local seclist=""
+function deploy_fromfile {
+    type=$1
+    
+    kubectl -n $ns create $type $objectname --from-file=$objectpath
+    RESULT=$?
 
-                for sec in $( find secrets/$ns/$pod/ -type f ); do
-                        seclist="--from-file=$sec $seclist"
-                done
-                kubectl -n $ns create secret generic $pod $seclist
-                RETURN=$?
-        
-                if [[ $RETURN -eq 1 ]]; then
-                        kubectl -n $ns delete secret $pod
-                        kubectl -n $ns create secret generic $pod $seclist
-
-                        sleep 0.5
-
-                        kubectl -n $ns patch deployment $pod -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"secretUpdate\":\"`date +'%s'`\"}}}}}"
-
-                elif [[ $RETURN -eq 0 ]]; then
-                        kubectl -n $ns patch deployment $pod -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"secretUpdate\":\"`date +'%s'`\"}}}}}"
-                else
-                        echo "UNKNOWN ERROR DURING SECRETS APPLY. ERROR $RETURN"
-                fi
-}
-
-function deployconfigmap {
-                local sec=""
-                local seclist=""
-
-                for sec in $( find configmaps/$ns/$pod/ -type f ); do
-                        seclist="--from-file=$sec $seclist"
-                done
-                kubectl -n $ns create configmap $pod $seclist
-                RETURN=$?
-        
-                if [[ $RETURN -eq 1 ]]; then
-                        kubectl -n $ns delete configmap $pod
-                        kubectl -n $ns create configmap $pod $seclist
-
-                        sleep 0.5
-
-                        kubectl -n $ns patch deployment $pod -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"configmapUpdate\":\"`date +'%s'`\"}}}}}"
-
-                elif [[ $RETURN -eq 0 ]]; then
-                        kubectl -n $ns patch deployment $pod -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"configmapUpdate\":\"`date +'%s'`\"}}}}}"
-                else
-                        echo "UNKNOWN ERROR DURING CONFIG MAP APPLY. ERROR $RETURN"
-                fi
+    if [[ $RESULT -ne 0 ]]; then
+        kubectl -n $ns delete $type $objectname
+        kubectl -n $ns create $type $objectname --from-file=$objectpath
+    fi
 }
 
 #set -x
 
 workspace=${1:-/src}
-oldlist=/tmp/oldlist
-newlist=/tmp/newlist
-donesec=()
-donemap=()
 
 lock
 
 # INITIAL LIST and CLONE
 cd $workspace
-find ! -path "./.git*" -type f -exec md5sum "{}" + > $oldlist
+
+for ns in $( git branch -r | grep -vE 'TEMPLATE|master' | awk -F "/" '{print $2}' ); do
+    
+    git checkout $ns
+    git pull
+
+    oldlist=/tmp/$ns\_oldlist
+    find ! -path "./.git*" -type f -exec md5sum "{}" + > $oldlist
+
+done
 
 while true; do
-    find ! -path "./.git*" -type f -exec md5sum "{}" + > $newlist
+    for ns in $( git branch -r | grep -vE 'TEMPLATE|master' | awk -F "/" '{print $2}' ); do 
+    
+      oldlist=/tmp/$ns\_oldlist
+      newlist=/tmp/$ns\_newlist
+     
+      git checkout $ns 
+      git pull  
 
-        for el in $( comm -1 -3 <(sort /tmp/oldlist) <(sort /tmp/newlist) | cut -d " " -f3 ); do
+      find ! -path "./.git*" -type f -exec md5sum "{}" + > $newlist
 
-                get_data $el
+      for item in $( comm -1 -3 <(sort /tmp/$ns\_oldlist) <(sort /tmp/$ns\_newlist) | cut -d " " -f3 ); do
+        get_data $item
 
-                case "$el" in 
-                        *namespaces*)
-                                kubectl create -f $el 
-                                ;;
-                        *deployments*)
-                                kubectl apply -f $el 
-                                ;;
-                        *secrets*)
-                                if [[ "${donesec[@]}" != *"$pod"* ]]; then
-                                        deploysecrets $el
-                                fi
-                                donesec+=($pod)
-                                ;;
-                        *configmaps*) 
-                                if [[ "${donemap[@]}" != *"$pod"* ]]; then
-                                        deployconfigmap $el
-                                fi
-                                donemap+=($pod)
-                                ;;
+        case "$item" in 
+          *deployments*)
+             kubectl -n $ns apply -f $item
+             ;;
+          *secrets*)
+             if [[ "$item" == *".yaml" ]]; then
+               kubectl -n $ns apply -f $item
+             elif [[ "${objectdone[@]}" != $objectpath ]]; then
+               deploy_fromfile secret
+             fi
+             objectdone+=($objectpath)
+             ;;
+          *configmaps*)
+             if [[ "$item" == *".yaml" ]]; then
+               kubectl -n $ns apply -f $item
+             elif [[ "${objectdone[@]}" != $objectpath ]]; then
+               deploy_fromfile configmap
+             fi
+             objectdone+=($objectpath)
+             ;;
+          *statefulsets*)
+             kubectl -n $ns apply -f $item
+             ;;
+          *services*)
+             kubectl -n $ns apply -f $item
+             ;;
+           *)
+             echo "Unhandled file $item"
+             ;;
+         esac
+      done
 
-                        *)
-                                echo "Unhandled file $el"
-                                ;;
-                esac
-        done
+      cp $newlist $oldlist
+      objectdone=()
 
-        cp $newlist $oldlist
-        donesec=()
-        donemap=()
-
-        sleep 10
+    done
+  sleep 10
 done
 
 remove_lock
